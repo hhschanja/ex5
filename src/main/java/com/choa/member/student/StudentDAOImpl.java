15d031782f00371f34a2e452a9b64137e1d2ddee
user2-PC\user2 <hhschanja@hanmail.net>
2017-06-21 15:54:40 +0900

    dd

---
+++ src/main/java/com/choa/member/student/StudentDAOImpl.java
@@ -0,0 +1,14 @@
+package com.choa.member.student;
+
+import com.choa.member.MemberDAO;
+import com.choa.member.MemberDTO;
+
+public class StudentDAOImpl implements MemberDAO{
+
+	@Override
+	public int memberJoin(MemberDTO memberDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
