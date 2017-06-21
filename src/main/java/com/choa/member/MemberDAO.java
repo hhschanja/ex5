825255829aeeb3880b7fbf5183421f7852dd3837
user2-PC\user2 <hhschanja@hanmail.net>
2017-06-21 15:59:41 +0900

    aa

--- src/main/java/com/choa/member/MemberDAO.java
+++ src/main/java/com/choa/member/MemberDAO.java
@@ -1,10 +1,7 @@
 package com.choa.member;
 
-public class MemberDAO {
-
 public interface MemberDAO {
 	
-	public int memberJoin(MemberDTO memberDTO) throws Exception;
+	public int memberJoin(MemberDTO memberDTO)throws Exception;
 	
->>>>>>> member
 }
--- src/main/java/com/choa/member/teacher/TeacherDAOImpl.java
+++ src/main/java/com/choa/member/teacher/TeacherDAOImpl.java
@@ -1,5 +1,14 @@
 package com.choa.member.teacher;
 
-public class TeacherDAOImpl {
+import com.choa.member.MemberDAO;
+import com.choa.member.MemberDTO;
+
+public class TeacherDAOImpl implements MemberDAO{
+
+	@Override
+	public int memberJoin(MemberDTO memberDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
