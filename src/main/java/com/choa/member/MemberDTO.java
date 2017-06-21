bfd3cc4ddbe3a0557978b4042bf38846ea82406a
user2-PC\user2 <hhschanja@hanmail.net>
2017-06-21 15:36:27 +0900

    jhuu

--- src/main/java/com/choa/member/MemberDTO.java
+++ src/main/java/com/choa/member/MemberDTO.java
@@ -18,7 +18,7 @@ public class MemberDTO {
 	public String getPw() {
 		return pw;
 	}
-	public void setPw(String pw) {
+	public void setPw(String pw) { 
 		this.pw = pw;
 	}
 	public String getName() {
